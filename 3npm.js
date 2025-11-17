8a62634dd803977f12bc2035b54bf139a32c7c98
Shani Patel <shanipatel37780@gmail.com>
2025-11-17 22:34:55 +0530

    NPM
    basic point

---
+++ 3npm.js
@@ -0,0 +1,10 @@
+// NPM understanding
+
+// NPMnode package manager
+// install ->npm i
+// installing and uninstalling anything basic & advanced
+// understanding node_modules
+// dependencies
+// devdependencies
+// script - understanding default script PATH and custom scripts
+//  npm i one-liner-joke
